9853157873c1dc05d2111d6a644305ee84409f71
J0T4J <juanvleal@gmail.com>
2022-10-21 08:11:37 -0300

    Convert gradle project to maven

--- src/main/java/com/todocalendar/services/TodoService.java
+++ src/main/java/com/todocalendar/services/TodoService.java
@@ -1,10 +1,16 @@
 package com.todocalendar.services;
 
+import com.todocalendar.dto.TodoDto;
+import com.todocalendar.form.TodoForm;
+import com.todocalendar.models.Todoitem;
 import com.todocalendar.repositories.TodoRepository;
+import com.todocalendar.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -12,7 +18,20 @@ public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
 
+    @Transactional
+    public TodoDto insert(TodoForm form){
+        Todoitem entity = new Todoitem();
+        entity.setTitle(form.getTitle());
+        entity.setDescription(form.getDescription());
+        entity.setDone(form.isDone());
+        entity = todoRepository.save(entity);
+        return new TodoDto(entity);
+    }
 
+    public TodoDto findById(Long id){
+        Optional<Todoitem> obj= todoRepository.findById(id);
+        Todoitem entity = obj.orElseThrow(() -> new ResourceNotFoundException()
+    }
 
 
 }
